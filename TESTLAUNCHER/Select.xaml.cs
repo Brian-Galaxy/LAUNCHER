b112cfedd5edca4e5100fddb19b527eca1871301
Brian Galaxy <mohovikov.denis@gmail.com>
2021-07-11 03:26:30 +0300

    Добавьте файлы проекта.

---
+++ TESTLAUNCHER/Select.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TESTLAUNCHER
+{
+    /// <summary>
+    /// Логика взаимодействия для Select.xaml
+    /// </summary>
+    public partial class Select : Window
+    {
+        public Select()
+        {
+            InitializeComponent();
+            AutoUpdater.Start("https://pastebin.com/raw/WRR00DxV");
+
+        }
+    }
+}
