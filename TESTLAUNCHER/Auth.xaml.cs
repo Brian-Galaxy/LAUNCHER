423d8570a183653bb04d9c42955f903d1c4d59e4
Brian Galaxy <mohovikov.denis@gmail.com>
2021-07-12 13:54:13 +0300

    ⨠Эксперимент с дизайном

---
+++ TESTLAUNCHER/Auth.xaml.cs
@@ -0,0 +1,41 @@
+﻿using AutoUpdaterDotNET;
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace TESTLAUNCHER
+{
+    public partial class Select : Window
+    {
+
+        private System.Media.SoundPlayer SoundPlayer;
+        public Select()
+        {
+            InitializeComponent();
+            AutoUpdater.Start("https://pastebin.com/raw/WRR00DxV");
+            checkBox.IsEnabled = Properties.Settings.Default.remCheck;
+
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void image_Copy_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SoundPlayer = new System.Media.SoundPlayer();
+            string back = Environment.CurrentDirectory + "\\Sounds\\back.wav";
+            SoundPlayer.SoundLocation = back;
+            SoundPlayer.Load();
+            SoundPlayer.Play();
+        }
+
+        private void checkBox_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Properties.Settings.Default.remCheck = checkBox.IsChecked.Value;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
--- TESTLAUNCHER/Properties/Settings.Designer.cs
+++ TESTLAUNCHER/Properties/Settings.Designer.cs
@@ -13,13 +13,49 @@ namespace TESTLAUNCHER.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string login {
+            get {
+                return ((string)(this["login"]));
+            }
+            set {
+                this["login"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string pass {
+            get {
+                return ((string)(this["pass"]));
+            }
+            set {
+                this["pass"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool remCheck {
+            get {
+                return ((bool)(this["remCheck"]));
+            }
+            set {
+                this["remCheck"] = value;
             }
         }
     }
