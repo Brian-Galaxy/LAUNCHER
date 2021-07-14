70307e5ce7915227eb96c5caac1d28b799f25cd3
Brian Galaxy <mohovikov.denis@gmail.com>
2021-07-14 18:50:47 +0300

    Авторизация 😎

--- TESTLAUNCHER/Auth.xaml.cs
+++ TESTLAUNCHER/Auth.xaml.cs
@@ -1,13 +1,15 @@
 ﻿using AutoUpdaterDotNET;
+using MySql.Data.MySqlClient;
 using System;
+using System.Data;
 using System.Threading;
 using System.Windows;
+using System.Windows.Input;
 
 namespace TESTLAUNCHER
 {
     public partial class Select : Window
     {
-
         private System.Media.SoundPlayer SoundPlayer;
         public Select()
         {
@@ -20,7 +22,34 @@ namespace TESTLAUNCHER
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            String loginUser = loginBox.Text;
+            String passUser = passwordBox.Password;
 
+            DB db = new DB();
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login`= @_login AND `pass`= @_pass", db.getConnection());
+
+            command.Parameters.Add("@_login", MySqlDbType.VarChar).Value = loginUser;
+            command.Parameters.Add("@_pass", MySqlDbType.VarChar).Value = passUser;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+            /*if (inCheck.Checked == true)
+            {
+                Properties.Settings.Default.username = loginUser;
+                Properties.Settings.Default.password = passUser;
+            }*/
+            if (table.Rows.Count > 0)
+            {
+                MessageBox.Show("Auth Complete");
+            }
+            else
+            {
+                MessageBox.Show("Failure");
+            }
         }
 
         private void image_Copy_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -36,6 +65,19 @@ namespace TESTLAUNCHER
         {
             Properties.Settings.Default.remCheck = checkBox.IsChecked.Value;
             Properties.Settings.Default.Save();
+        }
+
+        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void image1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
         }
     }
 }
